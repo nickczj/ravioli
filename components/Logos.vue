ff68cd2b66a10f4bd882cebed7ff3a7821c8aac1
nickczj <nick.chow.zj@gmail.com>
2022-08-22 15:58:07 +0800

    make logo size responsive

--- components/Logos.vue
+++ components/Logos.vue
@@ -1,7 +1,7 @@
 <template>
   <div inline-flex text-2xl font-300 cursor-default>
     <div flex flex-col children:mx-auto>
-      <img w-100 h-50 inline-block src="/logo.png">
+      <img w-60 h-30 lg:w-100 lg:h-50 inline-block src="/logo.png">
     </div>
   </div>
 </template>
